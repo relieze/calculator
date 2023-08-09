8a99a9efd38f39406366f11474920c3b97d8dc15
relieze <127432944+relieze@users.noreply.github.com>
2023-08-09 12:09:19 -0700

    Add function: gives the answer of an expression

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,15 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+num1 = prompt("num1:")
+operator = prompt("operator:")
+num2 = prompt("num2:")
+
+operate(num1, num2, operator);
+
 function add(num1, num2) {
-  return num1 + num2;
+  return +num1 + +num2;
 }
 
 function subtract(num1, num2) {
@@ -14,3 +24,17 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+console.log(num1 + " " + operator + " " + num2);
+
+function operate(num1, num2, operator) {
+  switch(operator) {
+    case("+"):  console.log(add(num1, num2));
+                break;
+    case("-"):  console.log(subtract(num1, num2));
+                break;
+    case("*"):  console.log(multiply(num1, num2));
+                break;
+    case("/"):  console.log(divide(num1, num2));
+                break;
+  }
+}
