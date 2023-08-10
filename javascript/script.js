c0b4900199a3a9ebe1a994ffc79eb5b406f12cbb
relieze <127432944+relieze@users.noreply.github.com>
2023-08-10 01:28:03 -0700

    Add event for equals sign; reset calc after answer

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("h1");
 const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
 const numberbtns = document.querySelectorAll(".number");
 const operatorbtns = document.querySelectorAll(".operators button");
 const equalsbtn = document.querySelector(".equals");
@@ -7,29 +8,45 @@ const equalsbtn = document.querySelector(".equals");
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+let answer = "";
 
 numberbtns.forEach(btn => {
-  btn.addEventListener("click", event => {
-    if (screen.textContent.length < 9 && operator === "") {
-      screen.textContent += event.target.textContent;
+  btn.addEventListener("click", () => {
+    if (equalsbtn.classList.contains("on")) {
+      equalsbtn.classList.remove("on");
+      screen.textContent = btn.textContent;
+      num1 = +screen.textContent;
+    } else if (screen.textContent.length < 9 && operator === "") {
+      screen.textContent += btn.textContent;
       num1 = +screen.textContent;
     } else if (screen.textContent === operator) {
-      screen.textContent = event.target.textContent;
+      screen.textContent = btn.textContent;
       num2 = +screen.textContent;
     } else if (screen.textContent.length < 9) {
-      screen.textContent += event.target.textContent;
+      screen.textContent += btn.textContent;
       num2 = +screen.textContent;
     }
   })
 })
 
 operatorbtns.forEach(btn => {
-  btn.addEventListener("click", event => {
-    if (num2 === 0) {
-      screen.textContent = event.target.textContent;
+  btn.addEventListener("click", () => {
+    if (num1 != 0 && num2 === 0) {
+      screen.textContent = btn.textContent;
       operator = btn.textContent;
     }
   })
+})
+
+equalsbtn.addEventListener("click", () => {
+  if (num1 && num2 && operator) {
+    answer = operate(num1, num2, operator);
+    screen.textContent = answer;
+    equalsbtn.classList.toggle("on");
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+  }
 })
 
 function add(num1, num2) {
@@ -48,13 +65,19 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-header.textContent = num1 + " " + operator + " " + num2; // just to see, not permanent
-
 function operate(num1, num2, operator) {
   switch(operator) {
     case("+"):  return add(num1, num2);
     case("-"):  return subtract(num1, num2);
-    case("*"):  return multiply(num1, num2);
-    case("/"):  return divide(num1, num2);
+    case("x"):  return multiply(num1, num2);
+    case("÷"):  return divide(num1, num2);
   }
 }
+
+// just to see, for dubugging purposes, not permanent
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    header.textContent = num1 + " " + operator + " " + num2 + " = " + answer; 
+  });
+})
